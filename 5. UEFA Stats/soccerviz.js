477cfc86b48ac10354949e43d03846cbae9c8102
kronosGR <geo.elgeo@gmail.com>
2022-07-12 13:43:08 +0200

    few improvements

--- 5. UEFA Stats/soccerviz.js
+++ 5. UEFA Stats/soccerviz.js
@@ -66,18 +66,41 @@ function overallTeamViz(incomingData) {
       .attr("r", function (d) {
         return radiusScale(d[datapoint]);
       });
+
+    teamG
+      .append("circle")
+      .attr("r", 0)
+      .transition()
+      .delay((d, i) => i * 100)
+      .duration(500)
+      .attr("r", 40)
+      .transition()
+      .duration(500)
+      .attr("r", 20);
   }
 
   teamG.on("mouseover", highlightRegion);
-  teamG.on("mouseout", function () {
-    d3.selectAll("g.overallG").select("circle").style("fill", "pink");
-  });
+  teamG.on("mouseout", unHighlight);
 
-  function highlightRegion(d) {
+  function unHighlight() {
+    d3.selectAll("g.overallG").select("circle").attr("class", "");
     d3.selectAll("g.overallG")
+      .select("text")
+      .classed("active", false)
+      .attr("y", 30);
+  }
+
+  function highlightRegion(d, i) {
+    d3.select(this).select("text").classed("active", true).attr("y", 10);
+
+    d3.selectAll("g.overallg")
       .select("circle")
-      .style("fill", function (p) {
-        return p.region == d.region ? "red" : "gray";
+      .each((p) => {
+        p.region == d.region
+          ? d3.select(this).classed("active", true)
+          : d3.select(this).classed("inactive", true);
       });
+
+    this.parentElement.appendChild(this);
   }
 }
