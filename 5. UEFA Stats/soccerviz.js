b4ea0d714fb2c246633c6b5d2d173b52849fb5e1
kronosGR <geo.elgeo@gmail.com>
2022-07-12 13:31:54 +0200

    added animation

--- 5. UEFA Stats/soccerviz.js
+++ 5. UEFA Stats/soccerviz.js
@@ -1,18 +1,8 @@
-function createSoccerViz() {
-  d3.csv("worldcup.csv").then((data) => {
-    overallTeamViz(data);
-  });
-}
+d3.csv("worldcup.csv").then((data) => {
+  overallTeamViz(data);
+});
 
 function overallTeamViz(incomingData) {
-  const buttonClick = (datapoint) => {
-    const maxValue = d3.max(incomingData, (d) => parseFloat(d[datapoint]));
-    const radiusScale = d3.scaleLinear().domain([0, maxValue]).range([2, 20]);
-    d3.selectAll("g.overallG")
-      .select("circle")
-      .attr("r", (d) => radiusScale(d[datapoint]));
-  };
-
   d3.select("svg")
     .append("g")
     .attr("id", "teamsG")
@@ -22,23 +12,72 @@ function overallTeamViz(incomingData) {
     .enter()
     .append("g")
     .attr("class", "overallG")
-    .attr("transform", (d, i) => `translate(${i * 50}, 0)`);
+    .attr("transform", function (d, i) {
+      return "translate(" + i * 50 + ", 0)";
+    });
 
-  const teamG = d3.selectAll("g.overallG");
-  teamG.append("circle").attr("r", 20);
+  var teamG = d3.selectAll("g.overallG");
+
+  teamG
+    .append("circle")
+    .attr("r", 0)
+    .transition()
+    .delay(function (d, i) {
+      return i * 100;
+    })
+    .duration(500)
+    .attr("r", 40)
+    .transition()
+    .duration(500)
+    .attr("r", 20);
+
   teamG
     .append("text")
+    .style("text-anchor", "middle")
     .attr("y", 30)
-    .text((d) => d.team);
+    .style("font-size", "10px")
+    .text(function (d) {
+      return d.team;
+    });
 
   const dataKeys = Object.keys(incomingData[0]).filter(
     (d) => d !== "team" && d !== "region"
   );
+
   d3.select("#controls")
     .selectAll("button.teams")
     .data(dataKeys)
     .enter()
     .append("button")
     .on("click", buttonClick)
-    .html((d) => d);
+    .html(function (d) {
+      return d;
+    });
+
+  function buttonClick(datapoint) {
+    var maxValue = d3.max(incomingData, function (d) {
+      return parseFloat(d[datapoint]);
+    });
+    var radiusScale = d3.scaleLinear().domain([0, maxValue]).range([2, 20]);
+    d3.selectAll("g.overallG")
+      .select("circle")
+      .transition()
+      .duration(1000)
+      .attr("r", function (d) {
+        return radiusScale(d[datapoint]);
+      });
+  }
+
+  teamG.on("mouseover", highlightRegion);
+  teamG.on("mouseout", function () {
+    d3.selectAll("g.overallG").select("circle").style("fill", "pink");
+  });
+
+  function highlightRegion(d) {
+    d3.selectAll("g.overallG")
+      .select("circle")
+      .style("fill", function (p) {
+        return p.region == d.region ? "red" : "gray";
+      });
+  }
 }
